9f49e0753cfd255ceb8bb070e88aba2f12ee9562
Vijay Shankar <vijayshankarmb@gmail.com>
2026-05-26 08:20:03 +0530

    dockerize FastAPI AI backend

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -10,7 +10,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL not found in environment")
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
